7c3ac8437a2c5bb6d9f15bea4eca390a74229452
Vinayak <Vinayaksinghforyou@gmail.com>
2025-07-25 01:39:02 +0530

    readme finished

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.le73lifnesg"
+    "revision": "0.muq3f7h8rg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
