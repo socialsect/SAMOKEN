8196d28280273cb864b34e1ad6ef03ad21143494
Vinayak <Vinayaksinghforyou@gmail.com>
2025-07-25 04:13:17 +0530

    ey

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5n1ntl23p7"
+    "revision": "0.36te3cc9h0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
