e526f9ccb6ca91ecbc4ca5fbc698a3813e6c407b
Vinayak <Vinayaksinghforyou@gmail.com>
2025-07-29 02:16:40 +0530

    posture detector made more accurate and production ready

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vecbbthm5j"
+    "revision": "0.l34a8n8rioo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
