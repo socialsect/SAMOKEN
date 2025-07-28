d7beb72e31f5b1c2c56b870ae3290be97bb9723a
Vinayak <Vinayaksinghforyou@gmail.com>
2025-07-29 02:41:12 +0530

    WIX AUTHENTICATION :DONE

--- src/wixClient.js
+++ src/wixClient.js
@@ -3,12 +3,12 @@ import { createClient, OAuthStrategy } from "@wix/sdk";
 import { members } from "@wix/members";
 import { redirects } from "@wix/redirects";
 const cid = "ae6977a5-70fe-403a-80f0-58809d4cfcf6";
-const redirectUri = "http://localhost:5173/auth/callback"; // Ensure this matches your app's registered redirect URI
+const redirectUri = "https://runner-orpin.vercel.app/auth/callback"; // Ensure this matches your app's registered redirect URI
 
 const wixClient = createClient({
   modules: { members, redirects },
   auth: OAuthStrategy({
-    clientId: cid,
+    clientId: cid,  
     redirectUri: redirectUri,
   }),
 });
